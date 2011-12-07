e0d17deb1dab3eab88565961d29406b2e4b36c7a
Jan Dudek <jd@jandudek.com>
2011-12-07 15:29:21 +0100

    Removed unnecessary to_sym for methods names

--- lib/has_crud_for.rb
+++ lib/has_crud_for.rb
@@ -38,27 +38,27 @@ module HasCrudFor
   def define_crud_methods(models, name, methods = [])
     mixin = has_crud_for_methods
 
-    mixin.send :define_method, "find_#{name}".to_sym do |id|
+    mixin.send :define_method, "find_#{name}" do |id|
       send(models).find(id)
     end if methods.include?(:find)
 
-    mixin.send :define_method, "build_#{name}".to_sym do |hash = {}|
+    mixin.send :define_method, "build_#{name}" do |hash = {}|
       send(models).build(hash)
     end if methods.include?(:build)
 
-    mixin.send :define_method, "create_#{name}".to_sym do |hash = {}|
+    mixin.send :define_method, "create_#{name}" do |hash = {}|
       send(models).create(hash)
     end if methods.include?(:create)
 
-    mixin.send :define_method, "create_#{name}!".to_sym do |hash = {}|
+    mixin.send :define_method, "create_#{name}!" do |hash = {}|
       send(models).create!(hash)
     end if methods.include?(:create)
 
-    mixin.send :define_method, "update_#{name}".to_sym do |id, hash|
+    mixin.send :define_method, "update_#{name}" do |id, hash|
       send(models).update(id, hash)
     end if methods.include?(:update)
 
-    mixin.send :define_method, "destroy_#{name}".to_sym do |id|
+    mixin.send :define_method, "destroy_#{name}" do |id|
       send(models).destroy(id)
     end if methods.include?(:destroy)
   end
@@ -67,27 +67,27 @@ module HasCrudFor
     model_name = ActiveSupport::Inflector.singularize(models.to_s)
     mixin = has_crud_for_methods
 
-    mixin.send :define_method, "find_#{name}".to_sym do |parent_id, id|
+    mixin.send :define_method, "find_#{name}" do |parent_id, id|
       send("find_#{parent}", parent_id).send("find_#{model_name}", id)
     end if methods.include?(:find)
 
-    mixin.send :define_method, "build_#{name}".to_sym do |parent_id, hash = {}|
+    mixin.send :define_method, "build_#{name}" do |parent_id, hash = {}|
       send("find_#{parent}", parent_id).send("build_#{model_name}", hash)
     end if methods.include?(:build)
 
-    mixin.send :define_method, "create_#{name}".to_sym do |parent_id, hash = {}|
+    mixin.send :define_method, "create_#{name}" do |parent_id, hash = {}|
       send("find_#{parent}", parent_id).send("create_#{model_name}", hash)
     end if methods.include?(:create)
 
-    mixin.send :define_method, "create_#{name}!".to_sym do |parent_id, hash = {}|
+    mixin.send :define_method, "create_#{name}!" do |parent_id, hash = {}|
       send("find_#{parent}", parent_id).send("create_#{model_name}!", hash)
     end if methods.include?(:create)
 
-    mixin.send :define_method, "update_#{name}".to_sym do |parent_id, id, hash|
+    mixin.send :define_method, "update_#{name}" do |parent_id, id, hash|
       send("find_#{parent}", parent_id).send("update_#{model_name}", id, hash)
     end if methods.include?(:update)
 
-    mixin.send :define_method, "destroy_#{name}".to_sym do |parent_id, id|
+    mixin.send :define_method, "destroy_#{name}" do |parent_id, id|
       send("find_#{parent}", parent_id).send("destroy_#{model_name}", id)
     end if methods.include?(:destroy)
   end
